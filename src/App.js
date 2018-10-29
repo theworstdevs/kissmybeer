1cb0f237e0ed761f24cf4a1d98aa940f851686e4
lukjaki <l.jakimow@gmail.com>
2018-10-29 13:41:09 +0100

    merged

--- src/App.js
+++ src/App.js
@@ -2,34 +2,13 @@ import React, { Component } from 'react';
 import ProductPage from './ProductPage';
 
 class App extends Component {
-<<<<<<< HEAD
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-=======
+
     render() {
         return (
             <ProductPage/>
         );
     }
->>>>>>> 229c2774838ca26e28d6050d7d03148b4c2199ee
+
 }
 
 export default App;
