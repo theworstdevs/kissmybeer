ec9af8ada1b0ad345c1b7519da4501d51f716b50
Miłosz Kaczmarek <miloszkaczmarek@hotmail.com>
2018-10-29 14:01:24 +0100

    Homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class ProductPage extends Component {
+    render() {
+        return (
+            <h1>Mój wuja</h1>
+        );
+    }
+}
+
+export default ProductPage;
